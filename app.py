ff791809cfb3671415206f31bcf6ab81d1b578d2
Chukwueze Simon Delali <83236698+Simondelali@users.noreply.github.com>
2025-05-24 08:45:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🎉 Hello from Flask Frontend!"
+    return "🎉 Helloo from Flask Frontend!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
